c3bb17718c4bb0626bbf73b90739de6ca9e9c12c
Matěj Štaif <staifm@staifm.local>
2025-08-11 13:39:54 +0200

    changes

--- mortgage_approval_bayesian_network/data_generation_realistic.py
+++ mortgage_approval_bayesian_network/data_generation_realistic.py
@@ -2,6 +2,7 @@ import csv
 import os
 import random
 import time
+import math
 
 
 def encode_age_group(age):
@@ -29,7 +30,7 @@ class DataGenerator:
                                        "highest_education",
                                        "employment_type",
                                        "len_employment",
-                                       "study_status"
+                                       "study_status",
                                        "size_of_company",
                                        "investments",
                                        "investments_value",
@@ -59,6 +60,21 @@ class DataGenerator:
 
     def generate_random_float_from_0_to_1(self):
         return random.random()
+
+    def generate_base_value_property(self, reported_monthly_income):
+        base_value_property = reported_monthly_income * self.generate_random_int_from_x_to_y(100, 270)
+        base_value_property = math.ceil(base_value_property / 1000) * 1000
+        return base_value_property
+
+    def generate_random_investments(self, age, reported_monthly_income):
+        """
+        Investment simulation where a person saves 'realistic_investment_value' percent of
+        their monthly salary each month for 'investment_interval' years with 8% annual return.
+        """
+        investment_interval = age - 18
+        realistic_investment_value = self.generate_random_int_from_x_to_y(10,3000) / 100
+        savings_for_investments_per_year = reported_monthly_income * realistic_investment_value * 12
+        return (savings_for_investments_per_year * (((1.08)**investment_interval-1)/0.08)*1.08) # SPY has historically 7-9% annually return.
 
     def generate_random_int_from_x_to_y(self, x_from, y_to):
         return random.randint(x_from, y_to)
@@ -207,6 +223,7 @@ class DataGenerator:
                         r = self.generate_random_float_from_0_to_1()
                         if r >= 0 and r < 0.97: return "no"
                         else: return "yes"
+                    else: return "no"
 
                 study_status = generate_study_status()
 
@@ -216,8 +233,18 @@ class DataGenerator:
                     max_salary = self.avg_salary * 10
                     base_salary = self.avg_salary
 
+                    if age < 18:
+                        return 0
+
                     if employment_type == "unemployed":
-                        return min_salary
+                        # unemployment benefit
+                        benefit = True if self.generate_random_float_from_0_to_1() > 0.7 else False
+                        if benefit is False:
+                            return 0
+                        if benefit is True:
+                            return self.generate_random_int_from_x_to_y(0, min_salary / 2.22)
+                        return 0
+
                     if study_status == "yes" and highest_education != "master" and highest_education != "bachelor":
                         return min_salary / 2
                     if study_status == "yes" and highest_education == "bachelor":
@@ -258,16 +285,128 @@ class DataGenerator:
                         mean *= 0.85
                         std_dev *= 0.7
 
-                    if employment_type == "freelance":
+                    if employment_type == "freelancer":
                         std_dev *= 1.8
 
-                    if employment_type == "permanent" and highest_education == "master" or highest_education == "phd" and len_employment > 20:
+                    if employment_type == "permanent" and (highest_education == "master" or highest_education == "phd") and len_employment > 20:
                         std_dev *= 0.5
 
                     # Generate salaries with Gaussian distribution
                     salary = random.gauss(mean, std_dev)
 
                     salary = max(min_salary, min(max_salary, salary))
-                    return int(round(salary / 100) * 100)
+                    return math.ceil(salary / 100) * 100
 
                 reported_monthly_income = generate_reported_monthly_income()
+
+                def generate_credit_history():
+                    # TODO for CLAUDE CODE: napis mi toto generate_credit_history, cim vyssi plat nad prumerem a cim vyssi vzdelani tim lepsi credit history vyhodnot, penalizuj velky dluh, penalizuj kdyz nema  clovek nemovitost ci investice (oboji dohromady ber jako celek) + odcen pokud pracuje pro stat (ale zde jen trosku  opravdu trosku!) odcen vzdelani hodne! + odcen stability income metriku
+
+                def generate_housing_status():
+                    if reported_monthly_income > self.avg_salary * 4:
+                        r = self.generate_random_float_from_0_to_1()
+                        if r >= 0 and r < 0.70: return "own"
+                        if r >= 0.7 and r < 0.95: return "mortgage"
+                        if r >= 0.95 and r <= 1.0: return "rent"
+
+                    if reported_monthly_income > self.avg_salary * 2:
+                        r = self.generate_random_float_from_0_to_1()
+                        if r >= 0 and r < 0.40: return "own"
+                        if r >= 0.4 and r < 0.90: return "mortgage"
+                        if r >= 0.90 and r <= 1.0: return "rent"
+
+                    if reported_monthly_income > self.avg_salary * 1.5:
+                        r = self.generate_random_float_from_0_to_1()
+                        if r >= 0 and r < 0.10: return "own"
+                        if r >= 0.1 and r < 0.60: return "mortgage"
+                        if r >= 0.60 and r <= 1.0: return "rent"
+
+                    if reported_monthly_income < self.avg_salary * 0.25:
+                        r = self.generate_random_float_from_0_to_1()
+                        if r >= 0 and r < 0.0001: return "own"
+                        if r >= 0.0001 and r < 0.0005: return "mortgage"
+                        if r >= 0.0005 and r <= 1.0: return "rent"
+
+                    if reported_monthly_income < self.avg_salary * 0.5:
+                        r = self.generate_random_float_from_0_to_1()
+                        if r >= 0 and r < 0.01: return "own"
+                        if r >= 0.01 and r < 0.09: return "mortgage"
+                        if r >= 0.09 and r <= 1.0: return "rent"
+
+                    r = self.generate_random_float_from_0_to_1()
+                    if r >= 0 and r < 0.02: return "own"
+                    if r >= 0.02 and r < 0.20: return "mortgage"
+                    if r >= 0.20 and r <= 1.0: return "rent"
+
+                housing_status = generate_housing_status()
+
+                def generate_property_owned_value():
+                    value = 0
+                    base_value_property = self.generate_base_value_property(reported_monthly_income)
+
+                    if housing_status == "own":
+                        value += base_value_property
+
+                    if housing_status == "mortgage":
+                        ownership = 0.3 + self.generate_random_float_from_0_to_1() * 0.4
+                        value += int(base_value_property * ownership)
+
+                    if reported_monthly_income > self.avg_salary * 4:
+                        if self.generate_random_float_from_0_to_1() > 0.85:
+                            value += self.generate_base_value_property(reported_monthly_income)
+
+                    if reported_monthly_income > self.avg_salary * 2 and reported_monthly_income < self.avg_salary * 4:
+                        if self.generate_random_float_from_0_to_1() > 0.98:
+                            value += self.generate_base_value_property(reported_monthly_income)
+
+                    if age > 55 and reported_monthly_income <= self.avg_salary * 2:
+                        if self.generate_random_float_from_0_to_1() > 0.98:
+                            value += self.generate_base_value_property(reported_monthly_income)
+                    return value
+
+                property_owned_value = generate_property_owned_value()
+
+                def generate_investments_value():
+                    if highest_education == "master" or highest_education == "phd":
+                        if self.generate_random_float_from_0_to_1() > 0.50:
+                            return self.generate_random_investments(age, reported_monthly_income)
+
+                    if highest_education == "bachelor":
+                        if self.generate_random_float_from_0_to_1() > 0.90:
+                            return self.generate_random_investments(age, reported_monthly_income)
+
+                    if reported_monthly_income > self.avg_salary * 1.25:
+                        if self.generate_random_float_from_0_to_1() > 0.90:
+                            return self.generate_random_investments(age, reported_monthly_income)
+
+                    if self.generate_random_float_from_0_to_1() > 0.98:
+                        return self.generate_random_investments(age, reported_monthly_income)
+
+                    return 0
+
+                investments_value = generate_investments_value()
+
+                def generate_investments():
+                    if investments_value > 0 or property_owned_value > 0:
+                        return "yes"
+                    return "no"
+
+                investments = generate_investments()
+
+                def generate_total_existing_debt():
+                    if housing_status == "mortgage":
+                        return property_owned_value * self.generate_base_value_property(reported_monthly_income) * self.generate_random_float_from_0_to_1()
+                    else:
+                        if self.generate_random_float_from_0_to_1() > 0.50:
+                            return property_owned_value * 0.8 * self.generate_base_value_property(reported_monthly_income) * self.generate_random_float_from_0_to_1()
+                    return 0
+
+                total_existing_debt = generate_total_existing_debt()
+
+                def generate_core_net_worth():
+                    net_worth = 0
+                    net_worth += property_owned_value + reported_monthly_income * 3 + investments_value - total_existing_debt
+                    return net_worth
+
+                core_net_worth = generate_core_net_worth()
+
